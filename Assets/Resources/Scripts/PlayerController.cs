0f50d026a2eab1cce67ccf28eb7eeaa6925e7d9c
SexyTsunami <ashyguy55@gmail.com>
2015-04-29 23:08:04 -0700

    added BoneSaw McGraw and why diagonal is broken

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -60,7 +60,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");	
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);////TIS MIGHT BE WHY IT IS NO SMOOTH GOING DIAGONALLY
 
 		transform.localEulerAngles = new Vector3 (20*moveVertical, 0, -80*moveHorizontal);
 
