62f6f75a7f68d1508e618a8ffa510d0800b41247
SexyTsunami <ashyguy55@gmail.com>
2015-05-03 12:57:45 -0700

    Added a shit load of buggy weapon content

--- Assets/Resources/Scripts/controllers/AlternatingWeaponController2.cs
+++ Assets/Resources/Scripts/controllers/AlternatingWeaponController2.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class AlternatingWeaponController: MonoBehaviour {
+public class AlternatingWeaponController2: MonoBehaviour {
 	//Look up renderer for unity
 
 	private float timePassed = 0;
@@ -20,14 +20,14 @@ public class AlternatingWeaponController: MonoBehaviour {
 		mainShot = shot1;
 	}
 
-//	void Update ()
-//	{
-//		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
-//			nextFire = Time.time + fireRate;
-//			GameObject player = GameObject.FindGameObjectWithTag ("Player");
-//			GameObject thing = (GameObject)Instantiate (mainShot, player.transform.position, new Quaternion (0, 0, 0, 0));
-//		}
-//	}
+	void Update ()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			GameObject player = GameObject.FindGameObjectWithTag ("Player");
+			GameObject thing = (GameObject)Instantiate (mainShot, player.transform.position, new Quaternion (0, 0, 0, 0));
+		}
+	}
 
 	void FixedUpdate()
 	{
@@ -41,7 +41,7 @@ public class AlternatingWeaponController: MonoBehaviour {
 			mainShot = shot2;
 		}
 
-		Instantiate (mainShot, transform.position, new Quaternion (0, 0, 0, 0));
+//		Instantiate (mainShot, transform.position, new Quaternion (0, 0, 0, 0));
 	
 
 	}
--- Assets/Resources/Scripts/movers/CosineSpinMover.cs
+++ Assets/Resources/Scripts/movers/CosineSpinMover.cs
@@ -1,42 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SineSpinMover : MonoBehaviour {
-
+public class CosineSpinMover : MonoBehaviour {
+	
 	public float speed;
 	private int rotationDirection;
 	private float timePassed = 0;
 	private Vector3 originalPosition;
 	public float amplitude;
 	private float x;
-
+	
 	void Start ()
 	{
 		originalPosition = GetComponent<Rigidbody> ().position;
-//		float sprayAngle = (Random.value * 2 - 1) * sprayAngleRange;
-//		Vector3 shootingDirection = new Vector3 (sprayAngle, 0, 1);
-//		GetComponent<Rigidbody>().velocity = shootingDirection * speed;
-//		this.rotationDirection = (Random.value - 0.5f > 0) ? 1 : -1;
+		//		float sprayAngle = (Random.value * 2 - 1) * sprayAngleRange;
+		//		Vector3 shootingDirection = new Vector3 (sprayAngle, 0, 1);
+		//		GetComponent<Rigidbody>().velocity = shootingDirection * speed;
+		//		this.rotationDirection = (Random.value - 0.5f > 0) ? 1 : -1;
 	}
-
+	
 	void FixedUpdate() {
 		timePassed += Time.deltaTime;
 		float y = timePassed * speed;
-		float x = amplitude*Mathf.Sin (y)+.05f;
-		float z = 45*Mathf.Cos (y);//derivative of x
+		float x = amplitude*Mathf.Cos (y)-.5f;
+		float z = -45*Mathf.Sin (y);//derivative of x
 		Vector3 angles = transform.localEulerAngles;
-
+		
 		Vector3 newPosition = originalPosition + new Vector3 (x, 0, y);
-
+		
 		GetComponent<Rigidbody> ().position = newPosition;
-				
+		
 		transform.localEulerAngles = new Vector3 (0, z, 0);
-
-//		Vector3 angles = transform.localEulerAngles;
-//
-//		Vector3 shootingDirection = new Vector3 (0, rotationDirection * 360 * time + GetComponent<Rigidbody> ().velocity.y, 0);
-//		GetComponent<Rigidbody>().velocity = shootingDirection * speed;
-
+		
+		//		Vector3 angles = transform.localEulerAngles;
+		//
+		//		Vector3 shootingDirection = new Vector3 (0, rotationDirection * 360 * time + GetComponent<Rigidbody> ().velocity.y, 0);
+		//		GetComponent<Rigidbody>().velocity = shootingDirection * speed;
+		
 		//transform.localEulerAngles = new Vector3 (0, rotationDirection*360*time + angles.y, 0);
 	}
 }
